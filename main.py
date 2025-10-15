4c6f22d8bec9aefa3bc4710cff78ca2b4c6c205d
Benlarry <4muki4@gmail.com>
2025-10-15 18:32:08 +0300

    added argv

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys
 from stats import count_words, get_book_text, count_characters, sort_dict
 
 
 def main():
-    PATH = "books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    PATH = sys.argv[1]
     text = get_book_text(PATH)
     sorted_characters_values = sort_dict(count_characters(text))
     
